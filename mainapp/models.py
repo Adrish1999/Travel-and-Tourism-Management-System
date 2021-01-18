a6a04f8c2e65eb8183570698e89e9bbda259d435
Adrish Bose <adrishbose198@gmail.com>
2021-01-18 21:57:51 +0530

    Templates added

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length = 100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank = True , null = True)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.views.generic import ListView , DetailView , View
+from django.utils import timezone
+from .models import Item
 
-# Create your views here.
+class HomeView(ListView):
+    model = Item
+    template_name = "index.html"
